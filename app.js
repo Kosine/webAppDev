a197ce36d0dc541d3801803bbc6d51f1527f3cf1
Sanjay Lindsay <Sanjay Lindsay>
2017-01-22 16:24:56 -0500

    Added interactivity

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var main = function () {
   "use strict";
 
   $(".comment-input button").on("click",function (event) {
-    alert("Hello world!");
+    var $new_comment;
+
+      if($(".comment-input input").val() !== ""){
+          $new_comment = $("<p>").text($(".comment-input input").val());
+          $(".comments").append($new_comment);
+    }
   });
-};
+}
 
 $(document).ready(main);
